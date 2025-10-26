05b257e6c59350e0878dad51dc151e24c9b6ea3b
Angur0 <crundee365@gmail.com>
2025-10-26 23:58:19 +0800

    feat: Add jQuery script inclusion to the common scripts section

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,4 +1,5 @@
 <?php /** Common scripts placed before </body> */ ?>
+<script src="../dist/jquery-3.7.1.min.js"></script>
 <script src="../dist/jquery.validate.min.js"></script>
 <script src="../dist/bootstrap.bundle.min.js"></script>
 <script src="../scripts/app.js"></script>

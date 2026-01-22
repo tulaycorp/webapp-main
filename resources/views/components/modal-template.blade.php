70f96ee6feeb12c08e776f19d87cfc7a707d9da9
notsonata <angelodm005@gmailcom>
2026-01-23 04:07:27 +0800

    fix: cors changes

--- resources/views/components/modal-template.blade.php
+++ resources/views/components/modal-template.blade.php
@@ -16,7 +16,7 @@
       <div class="p-8">
         <form id="login-form" class="space-y-6">
           <div>
-            <label for="login-email" class="text-label block mb-3">EMAIL ADDRESS *</label>
+            <label for="login-email" class="text-label block mb-3">EMAIL ADDRESS <span class="text-red-500">*</span></label>
             <input id="login-email" name="email" type="email" 
                    class="form-input text-base" 
                    required 
@@ -28,7 +28,7 @@
           </div>
           
           <div>
-            <label for="login-password" class="text-label block mb-3">PASSWORD *</label>
+            <label for="login-password" class="text-label block mb-3">PASSWORD <span class="text-red-500">*</span></label>
             <div class="relative">
               <input id="login-password" name="password" type="password" 
                      class="form-input text-base pr-12" 
@@ -94,7 +94,7 @@
         <form id="signup-form" class="space-y-6">
           <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div>
-              <label for="signup-first-name" class="text-label block mb-3">FIRST NAME *</label>
+              <label for="signup-first-name" class="text-label block mb-3">FIRST NAME <span class="text-red-500">*</span></label>
               <input id="signup-first-name" name="firstName" type="text" 
                      class="form-input text-base" 
                      required 
@@ -104,7 +104,7 @@
             </div>
             
             <div>
-              <label for="signup-last-name" class="text-label block mb-3">LAST NAME *</label>
+              <label for="signup-last-name" class="text-label block mb-3">LAST NAME <span class="text-red-500">*</span></label>
               <input id="signup-last-name" name="lastName" type="text" 
                      class="form-input text-base" 
                      required 
@@ -123,7 +123,7 @@
           </div>
           
           <div>
-            <label for="signup-email" class="text-label block mb-3">EMAIL ADDRESS *</label>
+            <label for="signup-email" class="text-label block mb-3">EMAIL ADDRESS <span class="text-red-500">*</span></label>
             <input id="signup-email" name="email" type="email" 
                    class="form-input text-base" 
                    required 
@@ -167,7 +167,7 @@
           </div>
           
           <div>
-            <label for="signup-password" class="text-label block mb-3">PASSWORD *</label>
+            <label for="signup-password" class="text-label block mb-3">PASSWORD <span class="text-red-500">*</span></label>
             <div class="relative">
               <input id="signup-password" name="password" type="password" 
                      class="form-input text-base pr-12" 
@@ -183,7 +183,7 @@
           </div>
           
           <div>
-            <label for="signup-password-confirm" class="text-label block mb-3">CONFIRM PASSWORD *</label>
+            <label for="signup-password-confirm" class="text-label block mb-3">CONFIRM PASSWORD <span class="text-red-500">*</span></label>
             <div class="relative">
               <input id="signup-password-confirm" name="password-confirm" type="password" 
                      class="form-input text-base pr-12" 
@@ -247,7 +247,7 @@
         
         <form id="forgot-form" class="space-y-6">
           <div>
-            <label for="forgot-email" class="text-label block mb-3">EMAIL ADDRESS *</label>
+            <label for="forgot-email" class="text-label block mb-3">EMAIL ADDRESS <span class="text-red-500">*</span></label>
             <input id="forgot-email" name="email" type="email" 
                    class="form-input text-base" 
                    required 
